7a5bc0c442156ef3aa4d378598003c5d7f409635
Jake Boxer <jake@github.com>
2014-04-15 00:05:38 -0700

    Add everything else

---
+++ NoHackerbots/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  NoHackerbots
+//
+//  Created by Jake Boxer on 4/15/14.
+//  Copyright Jake Boxer 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ NoHackerbots/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NoHackerbots
+//
+//  Created by Jake Boxer on 4/15/14.
+//  Copyright Jake Boxer 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
