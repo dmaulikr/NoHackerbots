4646e1a94ec1932be4b7e181fce2aa6c2af8318d
Jake Boxer <jake@github.com>
2014-05-10 23:35:04 -0700

    Add Board class

---
+++ NoHackerbots/Classes/Board.h
@@ -0,0 +1,13 @@
+//
+//  Board.h
+//  NoHackerbots
+//
+//  Created by Jake Boxer on 5/10/14.
+//  Copyright (c) 2014 Jake Boxer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Board : NSObject
+
+@end
---
+++ NoHackerbots/Classes/Board.m
@@ -0,0 +1,13 @@
+//
+//  Board.m
+//  NoHackerbots
+//
+//  Created by Jake Boxer on 5/10/14.
+//  Copyright (c) 2014 Jake Boxer. All rights reserved.
+//
+
+#import "Board.h"
+
+@implementation Board
+
+@end
